e98006d3464b246ef5c79399aa2247bc0bac3dbb
Eduardo Collazo <ecollazodominguez@gmail.com>
2021-12-21 12:22:34 +0100

    CRUD

---
+++ helpers/db-validators.js
@@ -0,0 +1,38 @@
+
+const Role = require('../models/role');
+const Usuario = require('../models/usuarios');
+
+ const esRoleValido = async(rol = '') => {
+    const existeRol = await Role.findOne({rol});
+    if (!existeRol){
+            throw new Error(`El rol ${rol} no está registrado en la BD`)
+    }
+}
+
+const emailExiste = async(correo = '') => {
+        // Verificar si el correo existe
+    const existeEmail = await Usuario.findOne({correo});
+    if (existeEmail){
+        throw new Error(`El correo ${correo} ya está registrado en la BD`)
+    }
+}
+
+const existeUsuarioPorId = async(id) => {
+        // Verificar si el correo existe
+    const existeUsuario = await Usuario.findById(id);
+    if (!existeUsuario){
+        throw new Error(`El id ${id} no existe`)
+    }
+}
+
+
+module.exports = {
+
+    esRoleValido,
+    emailExiste,
+    existeUsuarioPorId
+}
+
+
+
+
